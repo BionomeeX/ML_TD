a8e2fc3ecbbef701e0da276731433a70299c2912
Clement Carre <clement.carre@bionomeex.com>
2021-04-16 15:54:57 +0200

    Add linear algebra basic operations

---
+++ Assets/Scripts/ML/NN.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MLTD.ML
+{
+
+    public class Numeric{
+        public struct Vector {
+            public float[] data;
+            public int size;
+
+            public Vector(int size){
+                size = size;
+                data = new float[size];
+            }
+
+            public float At(int index){
+                return data[index];
+            }
+
+            public void At(int index, float value){
+                data[index] = value;
+            }
+
+            public void UpdateAt(int index, float value){
+                data[index] += value;
+            }
+
+        }
+
+        public class Matrix {
+            public float[] data;
+            public int nline;
+            public int ncolumn;
+
+            public Matrix(int nline, int ncol) {
+                nline = nline;
+                ncol = ncol;
+                data = new float[nline * ndata];
+            }
+
+            public float At(int indexLine, int indexColumn) {
+                return data[indexLine * ncolumn + indexColumn];
+            }
+
+            public void At(int indexLine, int indexColumn, float value) {
+                data[indexLine * ncolumn + indexColumn] = value;
+            }
+            public void UpdateAt(int indexLine, int indexColumn, float value) {
+                data[indexLine * ncolumn + indexColumn] += value;
+            }
+        }
+
+        public static Matrix MatMult(Matrix m1, Matrix m2) {
+            Matrix result = new Matrix(m1.nline, m2.ncolumn);
+            for(int line = 0; line < m1.nline; ++line) {
+                for(int col=0; col < m2.ncolumn; ++col) {
+                    for(int k = 0; k < m1.ncolumn; ++k) {
+                        result.UpdateAt(line, col, m1.At(line, k) * m2.At(k, col));
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static Vector MatMult(Matrix m, Vector v) {
+            Vector result = new Vector(m.nline);
+            for(int line = 0; line < m.nline; ++line) {
+                for(int col = 0; col < m.ncolumn; ++col) {
+                    result.UpdateAt(line, m.At(line, col) * v.At(col));
+                }
+            }
+            return result;
+        }
+
+        public static Vector SoftMax(Vector v){
+            Vector result = new Vector(v.size);
+            double total;
+            for(int i = 0; i < v.size; ++i){
+                float tmp = Mathf.Exp( v.At(i) );
+                result.At(i, tmp);
+                total += tmp;
+            }
+
+            for(int i = 0; i < v.size; ++i){
+                result.At(i, result.At(i) / total);
+            }
+
+            return result;
+        }
+
+        public static Vector Relu(Vector v){
+            Vector result = new Vector(v.size);
+            for(int i = 0; i < v.size; ++i){
+                result.At(i, v.At(i) > 0 ? v.At(i) : 0f);
+            }
+            return result;
+        }
+    }
+
+
+    public class NN
+    {
+        private int inputSize;
+        private int outputSize;
+        private List<int> hiddenLayers;
+
+        private List<Numeric.Matrix> weigths;
+
+        private
+        NN(int inputSize, int outputSize, List<int> hiddenLayers){
+
+        }
+
+        public float[] Forward(float[] data){
+
+            foreach(var matrix in weigths){
+
+            }
+
+        }
+    }
+
+
+}
