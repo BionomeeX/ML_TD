b7fbc2dc8fdcc65f6d3ab16944cbdbd29930336f
Xwilarg <christian.chaux@bionomeex.com>
2021-04-16 15:36:38 +0200

    Add input data parsing

--- Assets/Scripts/ML/Decision.cs
+++ Assets/Scripts/ML/Decision.cs
@@ -1,5 +1,6 @@
-﻿using MathNet.Numerics.LinearAlgebra;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MLTD.ML
 {
@@ -10,6 +11,22 @@ namespace MLTD.ML
             List<float> data = new List<float>();
             data.Add(input.Position.x / input.WorldSize.x);
             data.Add(input.Position.y / input.WorldSize.y);
+            data.Add(input.Direction);
+            data.Add(input.Speed);
+            foreach (var elem in input.RaycastInfos)
+            {
+                var max = Enum.GetValues(typeof(RaycastOutput)).Cast<int>().Max() + 1;
+                List<float> elems = new List<float>();
+                for (int i = 0; i < max; i++) elems.Add(0f);
+                elems[(int)elem.Item1] = elem.Item2 / input.RaycastMaxSize;
+            }
+            foreach (var msg in input.Messages)
+            {
+                List<float> m = new List<float>();
+                for (int i = 0; i < msg.Length; i++) m.Add(msg[i] ? 1f : 0f);
+            }
+            data.Add(input.CanUseSkill ? 1f : 0f);
+            data.Add(input.SkillTimer / input.SkillTimerMaxDuration);
             return data.ToArray();
         }
 
--- Assets/Scripts/ML/InputData.cs
+++ Assets/Scripts/ML/InputData.cs
@@ -9,10 +9,14 @@ namespace MLTD.ML
         public Vector2 Position; // Between 0 and 1
         public float Direction; // Direction between -1 and 1
         public float Speed; // Speed between -1 and 1
+
         public Tuple<RaycastOutput, float>[] RaycastInfos; // For each raycast, what the AI see and its distance
+        public float RaycastMaxSize;            
+
         public bool[][] Messages; // 5 messages that are a combinaison of 10 characters that are a message
 
         public bool CanUseSkill;
         public float SkillTimer;
+        public float SkillTimerMaxDuration;
     }
 }
