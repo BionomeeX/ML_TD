48560ebb343860ad0ee52ecd2cd13e04dc6fa8bb
Xwilarg <christian.chaux@bionomeex.com>
2021-04-19 15:41:33 +0200

    Add turret price

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,16 +2,23 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace MLTD.Enemy
+namespace MLTD.Player
 {
     public class PlayerController : MonoBehaviour
     {
+        public static PlayerController S;
+
+        private void Awake()
+        {
+            S = this;
+        }
+
         private Rigidbody2D _rb;
 
         [SerializeField]
         private Text _goldDisplay;
 
-        private int _goldAmount;
+        public int GoldAmount { set; get; }
 
         private const float _goldWaitTime = 3f;
         private const int _goldIncrease = 5;
@@ -31,13 +38,18 @@ namespace MLTD.Enemy
             _rb.velocity = new Vector2(hor, ver) * _speed;
         }
 
+        public void UpdateGoldText()
+        {
+            _goldDisplay.text = "Your Gold: " + GoldAmount;
+        }
+
         private IEnumerator IncreaseGold()
         {
             while (true)
             {
-                _goldDisplay.text = "Your Gold: " + _goldAmount;
+                _goldDisplay.text = "Your Gold: " + GoldAmount;
                 yield return new WaitForSeconds(_goldWaitTime);
-                _goldAmount += _goldIncrease;
+                GoldAmount += _goldIncrease;
             }
         }
     }
