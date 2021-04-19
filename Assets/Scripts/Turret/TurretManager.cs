5907405e592d4c63495bc9d31078c632c79d2611
Xwilarg <christian.chaux@bionomeex.com>
2021-04-19 18:32:51 +0200

    Add more documentation

--- Assets/Scripts/Turret/TurretManager.cs
+++ Assets/Scripts/Turret/TurretManager.cs
@@ -7,23 +7,29 @@ namespace MLTD.Turret
 {
     public class TurretManager : MonoBehaviour
     {
+        [Tooltip("Turret prefab")]
         [SerializeField]
         private GameObject _turret;
 
+        // List of all turrets instanciated
         private Dictionary<Vector2Int, GameObject> _turrets = new Dictionary<Vector2Int, GameObject>();
 
         public void Update()
         {
+            // When we click somewhere on the map
             if (Input.GetMouseButtonDown(0))
             {
+                // Set world position from mouse and convert it to grid position
                 var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 var dictPos = new Vector2Int(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y));
+
+                // If there is no turret in this position
                 if (!_turrets.Any(x => x.Key.x == dictPos.x && x.Key.y == dictPos.y)
-                    && PlayerController.S.GoldAmount >= 5)
+                    && PlayerController.S.GoldAmount >= 5) // Turret cost 5 gold
                 {
                     PlayerController.S.GoldAmount -= 5;
                     PlayerController.S.UpdateGoldText();
-                    var finalPos = new Vector2(dictPos.x + .5f, dictPos.y + .5f);
+                    var finalPos = new Vector2(dictPos.x + .5f, dictPos.y + .5f); // .5f is to put the turret on the right grid position
                     _turrets.Add(dictPos, Instantiate(_turret, finalPos, Quaternion.identity));
                 }
             }
